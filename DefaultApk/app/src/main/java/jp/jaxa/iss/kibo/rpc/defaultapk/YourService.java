2643dff037448451014ce49ca064f157dec6be2d
yusufpraditya <47532266+yusufpraditya@users.noreply.github.com>
2020-05-29 20:36:10 +0700

    Update YourService.java

--- DefaultApk/app/src/main/java/jp/jaxa/iss/kibo/rpc/defaultapk/YourService.java
+++ DefaultApk/app/src/main/java/jp/jaxa/iss/kibo/rpc/defaultapk/YourService.java
@@ -15,7 +15,6 @@ public class YourService extends KiboRpcService {
     // roll (x), pitch (y), yaw (z)
     private double roll, pitch, yaw;
     private double qua_w, qua_x, qua_y, qua_z;
-
     
     // OTW P1-1, orientasi naik 11.31 derajat
     private final Point POINT_1_P11 = new Point(10.95, -3.95, 4.85);
@@ -40,6 +39,7 @@ public class YourService extends KiboRpcService {
         final int LOOP_MAX = 3;
         arrayPoint = new Point[] {POINT_1_P11, POINT_2_P11, POINT_3_P11, POINT_4_P11, POINT_5_P11};
         arrayQuat = new double[][] {QUAT_1_P11_z, QUAT_2_P11_z, QUAT_3_P11_z, QUAT_4_P11_x, QUAT_5_P11_z};
+        
         for (int i = 0; i < arrayPoint.length; i++) {
             qua_w = 0;
             qua_x = 0;
@@ -47,11 +47,13 @@ public class YourService extends KiboRpcService {
             qua_z = 0;
             
             final Quaternion quaternion = eulerToQuaternion(arrayQuat[i]);
-            result = api.moveTo(arrayPoint[i], quaternion, true);
+            final Point point = arrayPoint[i];
+            result = api.moveTo(point, quaternion, true);
             // api.stopAllMotion();
+            
             int loopCounter = 0;
             while(!result.hasSucceeded() || loopCounter < LOOP_MAX){
-                result = api.moveTo(arrayPoint[i], quaternion, true);
+                result = api.moveTo(point, quaternion, true);
                 ++loopCounter;
             }
         }
@@ -70,12 +72,14 @@ public class YourService extends KiboRpcService {
     private Quaternion eulerToQuaternion(double[] rpy){
     	roll = Math.toRadians(rpy[0]);
     	pitch = Math.toRadians(rpy[1]);
-    	yaw = Math.toRadians(rpy[2]);        
+    	yaw = Math.toRadians(rpy[2]);     
+        
     	qua_x = Math.sin(roll / 2) * Math.cos(pitch / 2) * Math.cos(yaw / 2) - Math.cos(roll / 2) * Math.sin(pitch / 2) * Math.sin(yaw / 2);
     	qua_y = Math.cos(roll / 2) * Math.sin(pitch / 2) * Math.cos(yaw / 2) + Math.sin(roll / 2) * Math.cos(pitch / 2) * Math.sin(yaw / 2);
     	qua_z = Math.cos(roll / 2) * Math.cos(pitch / 2) * Math.sin(yaw / 2) - Math.sin(roll / 2) * Math.sin(pitch / 2) * Math.cos(yaw / 2);
     	qua_w = Math.cos(roll / 2) * Math.cos(pitch / 2) * Math.cos(yaw / 2) + Math.sin(roll / 2) * Math.sin(pitch / 2) * Math.sin(yaw / 2);
-    	Quaternion quat = new Quaternion((float)qua_x, (float)qua_y, (float)qua_z, (float)qua_w);
+    	
+        Quaternion quat = new Quaternion((float)qua_x, (float)qua_y, (float)qua_z, (float)qua_w);
     	return quat;
     }
 
