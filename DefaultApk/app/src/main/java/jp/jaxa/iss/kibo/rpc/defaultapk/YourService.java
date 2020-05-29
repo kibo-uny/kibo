0510c7b29c5f68a00e1f7d01530e918fea7d6c25
yusufpraditya <47532266+yusufpraditya@users.noreply.github.com>
2020-05-29 15:30:18 +0700

    Update YourService.java

--- DefaultApk/app/src/main/java/jp/jaxa/iss/kibo/rpc/defaultapk/YourService.java
+++ DefaultApk/app/src/main/java/jp/jaxa/iss/kibo/rpc/defaultapk/YourService.java
@@ -23,11 +23,11 @@ public class YourService extends KiboRpcService {
     private final Point POINT_3_P11 = new Point(11.28, -3.95, 4.85);
     private final Point POINT_4_P11 = new Point(11.28, -5.7, 4.5);
     private final Point POINT_5_P11 = new Point(11.28, -5.7, 4.5);
-    private double QUAT_1_P11_z = -90;
-    private double QUAT_2_P11_z = 0;
-    private double QUAT_3_P11_z = -90;
-    private double QUAT_4_P11_x = -11.31;
-    private double QUAT_5_P11_z = 0;
+    private double[] QUAT_1_P11_z = new double[]{0, 0, -90};
+    private double[] QUAT_2_P11_z = new double[]{0, 0, 0};
+    private double[] QUAT_3_P11_z = new double[]{0, 0, -90};
+    private double[] QUAT_4_P11_x = new double[]{-11.31, 0, 0};
+    private double[] QUAT_5_P11_z = new double[]{0, 0, 0};
 
     private Point[] arrayPoint = null;
     private Quaternion[] arrayQuaternion = null;
@@ -37,7 +37,7 @@ public class YourService extends KiboRpcService {
     protected void runPlan1(){
         // write here your plan 1
         Result result = null;
-        final int LOOP_MAX = 5;
+        final int LOOP_MAX = 3;
         arrayPoint = new Point[] {POINT_1_P11, POINT_2_P11, POINT_3_P11, POINT_4_P11, POINT_5_P11};
         arrayQuat = new double[] {QUAT_1_P11_z, QUAT_2_P11_z, QUAT_3_P11_z, QUAT_4_P11_x, QUAT_5_P11_z};
         for (int i = 0; i < arrayPoint.length; i++) {
@@ -45,20 +45,13 @@ public class YourService extends KiboRpcService {
             qua_x = 0;
             qua_y = 0;
             qua_z = 0;
-
-            if(arrayQuat[i] != -11.31) {
-                eulerToQuaternion(0, 0, arrayQuat[i]);
-            }
-            else {
-                eulerToQuaternion(arrayQuat[i], 0, 0);
-            }
-
-            final Quaternion quatesrnion = new Quaternion((float)qua_x, (float)qua_y, (float)qua_z, (float)qua_w);
+            
+            final Quaternion quaternion = eulerToQuaternion(arrayQuat[i]);
             result = api.moveTo(arrayPoint[i], quaternion, true);
             // api.stopAllMotion();
             int loopCounter = 0;
             while(!result.hasSucceeded() || loopCounter < LOOP_MAX){
-                result = api.moveTo(point, quaternion, true);
+                result = api.moveTo(arrayPoint[i], quaternion, true);
                 ++loopCounter;
             }
         }
@@ -74,15 +67,17 @@ public class YourService extends KiboRpcService {
         // write here your plan 3
     }
 
-	private void eulerToQuaternion(double r, double p, double y){
-        roll = Math.toRadians(r);
-        pitch = Math.toRadians(p);
-        yaw = Math.toRadians(y);
-        qua_w = Math.cos(roll / 2) * Math.cos(pitch / 2) * Math.cos(yaw / 2) + Math.sin(roll / 2) * Math.sin(pitch / 2) * Math.sin(yaw / 2);
+	private Quaternion eulerToQuaternion(double[] rpy){
+        roll = Math.toRadians(rpy[0]);
+        pitch = Math.toRadians(rpy[1]);
+        yaw = Math.toRadians(rpy[2]);        
         qua_x = Math.sin(roll / 2) * Math.cos(pitch / 2) * Math.cos(yaw / 2) - Math.cos(roll / 2) * Math.sin(pitch / 2) * Math.sin(yaw / 2);
         qua_y = Math.cos(roll / 2) * Math.sin(pitch / 2) * Math.cos(yaw / 2) + Math.sin(roll / 2) * Math.cos(pitch / 2) * Math.sin(yaw / 2);
         qua_z = Math.cos(roll / 2) * Math.cos(pitch / 2) * Math.sin(yaw / 2) - Math.sin(roll / 2) * Math.sin(pitch / 2) * Math.cos(yaw / 2);
-	}
+	    qua_w = Math.cos(roll / 2) * Math.cos(pitch / 2) * Math.cos(yaw / 2) + Math.sin(roll / 2) * Math.sin(pitch / 2) * Math.sin(yaw / 2);
+        Quaternion quat = new Quaternion((float)qua_x, (float)qua_y, (float)qua_z, (float)qua_w);
+	    return quat;
+    }
 
 }
 
