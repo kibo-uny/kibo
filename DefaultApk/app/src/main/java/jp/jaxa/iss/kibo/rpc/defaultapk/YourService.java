c102fdf3c59c47d313b9066b994611caee3d5440
yusufpraditya <47532266+yusufpraditya@users.noreply.github.com>
2020-06-10 19:18:39 +0700

    Update YourService.java

--- DefaultApk/app/src/main/java/jp/jaxa/iss/kibo/rpc/defaultapk/YourService.java
+++ DefaultApk/app/src/main/java/jp/jaxa/iss/kibo/rpc/defaultapk/YourService.java
@@ -1,10 +1,8 @@
 package jp.jaxa.iss.kibo.rpc.defaultapk;
 
+import jp.jaxa.iss.kibo.rpc.api.KiboRpcService;
+
 import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.ColorMatrix;
-import android.graphics.ColorMatrixColorFilter;
-import android.graphics.Paint;
 import android.util.Log;
 import android.graphics.Matrix;
 
@@ -18,16 +16,10 @@ import boofcv.alg.fiducial.qrcode.QrCode;
 import boofcv.android.ConvertBitmap;
 import boofcv.factory.fiducial.FactoryFiducial;
 import boofcv.struct.image.GrayU8;
-import gov.nasa.arc.astrobee.Robot;
-import jp.jaxa.iss.kibo.rpc.api.KiboRpcService;
+
 import gov.nasa.arc.astrobee.Result;
 import gov.nasa.arc.astrobee.types.Point;
 import gov.nasa.arc.astrobee.types.Quaternion;
-
-import net.sourceforge.zbar.Config;
-import net.sourceforge.zbar.Image;
-import net.sourceforge.zbar.ImageScanner;
-import net.sourceforge.zbar.Symbol;
 
 import org.opencv.aruco.Aruco;
 import org.opencv.aruco.Dictionary;
@@ -43,23 +35,22 @@ public class YourService extends KiboRpcService {
     private double qua_w, qua_x, qua_y, qua_z;
     private double pos_x, pos_y, pos_z;
     public static final String TAG = "TAG";
-    boolean TEST_FOUND = false;
     boolean AR_FOUND = false;
     int width = 1280;
     int height = 960;
 
     // OTW P1-1
     private final Point POINT_P11 = new Point(11.4, -5.7, 4.5);
-    private final double[] QUAT_P11 = new double[]{0, 0, -201.8}; //-201.8
+    private final double[] QUAT_P11 = new double[]{0, 0, -180}; //-201.8
 
     // OTW P1-3
     private final Point POINT_P13_1 = new Point(11, -5.5, 4.5);
     private final Point POINT_P13 = new Point(11, -5.5, 4.33);
-    private final double[] QUAT_P13_1 = new double[]{0, 0, -180};
+    private final double[] QUAT_P13_1 = new double[]{0, 0, -201.8};
     private final double[] QUAT_P13 = new double[]{0, -90, -180};
 
     // OTW P1-2
-    private final Point POINT_P12 = new Point(11, -6, 5.4);
+    private final Point POINT_P12 = new Point(11, -6, 5.35);
     private final double[] QUAT_P12 = new double[]{0, -90, -90};
 
     // LEWATI RINTANGAN 1
@@ -89,7 +80,7 @@ public class YourService extends KiboRpcService {
     private final double[] QUAT2_AVOID_2 = new double[]{0, 0, -90};
 
     private java.util.ArrayList<String> QR_LIST = new java.util.ArrayList<>();
-    private Robot robot;
+
     @Override
     protected void runPlan1(){
         // write here your plan 1
@@ -215,17 +206,46 @@ public class YourService extends KiboRpcService {
                 api.judgeSendDiscoveredAR(ar_id);
                 Log.d(TAG, "FOUND AR ID: " + ar_id);
                 Log.d(TAG, "FOUND AR ID: " + id);
-                api.laserControl(true);
+
                 double[] corner1 = corners.get(0).get(0,0);
                 double[] corner2 = corners.get(0).get(0,1);
                 double[] corner3 = corners.get(0).get(0,2);
                 double[] corner4 = corners.get(0).get(0,3);
 
+                if (corner1[0] < 640 && corner1[1] < 480) {
+                    // kiri atas
+                    Point PT_KIRI_ATAS = new Point(10.95, -9.63, 5.35);
+                    double[] QT_KIRI_ATAS  = new double[]{0, 15, -105};
+                    moveRobot(PT_KIRI_ATAS, QT_KIRI_ATAS);
+                }
+                else if (corner1[0] > 640 && corner1[1] < 480) {
+                    // kanan atas
+                    Point PT_KANAN_ATAS = new Point(10.95, -9.63, 5.35);
+                    double[] QT_KANAN_ATAS  = new double[]{0, 15, -75};
+                    moveRobot(PT_KANAN_ATAS, QT_KANAN_ATAS);
+                }
+                else if (corner1[0] < 640 && corner1[1] > 480) {
+                    // kiri bawah
+                    Point PT_KIRI_BAWAH = new Point(10.95, -9.63, 5.35);
+                    double[] QT_KIRI_BAWAH  = new double[]{0, -15, -105};
+                    moveRobot(PT_KIRI_BAWAH, QT_KIRI_BAWAH);
+                }
+                else if (corner1[0] > 640 && corner1[1] > 480) {
+                    // kanan bawah
+                    Point PT_KANAN_BAWAH = new Point(10.95, -9.63, 5.35);
+                    double[] QT_KANAN_BAWAH  = new double[]{0, -15, -75};
+                    moveRobot(PT_KANAN_BAWAH, QT_KANAN_BAWAH);
+                }
+
+                else {
+                    // do nothing
+                }
+
                 Log.d(TAG, "Corner 1: " + Arrays.toString(corner1));
                 Log.d(TAG, "Corner 2: " + Arrays.toString(corner2));
                 Log.d(TAG, "Corner 3: " + Arrays.toString(corner3));
                 Log.d(TAG, "Corner 4: " + Arrays.toString(corner4));
-
+                api.laserControl(true);
                 AR_FOUND = true;
             }
             else {
@@ -236,22 +256,22 @@ public class YourService extends KiboRpcService {
         }
     }
 
-    public void readQRBoofCV(int nomer, String camBoof) {
+    public void readQRBoofCV(int nomer, String cam) {
         int LOOP_CHECKQR_MAX = 4;
         int loopCheckQRCount = 0;
         int rotate = 0;
         boolean QR_FOUND = false;
         while (loopCheckQRCount < LOOP_CHECKQR_MAX && !QR_FOUND){
-            //selfPosition(nomer);
+            selfPosition(nomer);
             Bitmap bBoof = null;
-            if (camBoof.equals("dock")) {
+            if (cam.equals("dock")) {
                 bBoof = api.getBitmapDockCam();
                 Log.d(TAG, "BOOF: DEBUG 0 CHECK!");
                 if (rotate == 1) bBoof = RotateBitmap(bBoof, 1);
                 if (rotate == 2) bBoof = RotateBitmap(bBoof, 2);
                 if (rotate == 3) bBoof = RotateBitmap(bBoof, 3);
             }
-            else if (camBoof.equals("nav")) {
+            else if (cam.equals("nav")) {
                 bBoof = api.getBitmapNavCam();
                 Log.d(TAG, "BOOF: DEBUG 0 CHECK!");
                 if (rotate == 1) bBoof = RotateBitmap(bBoof, 1);
@@ -260,51 +280,39 @@ public class YourService extends KiboRpcService {
             }
             if (bBoof == null) Log.d(TAG, "BOOF: BITMAP IS NULL!");
             Bitmap resize = getResizedBitmap(bBoof, height/2, width/2);
-            try {
-                GrayU8 image = ConvertBitmap.bitmapToGray(resize, (GrayU8)null, null);
-                Log.d(TAG, "BOOF: DEBUG 1 CHECK!");
-                Log.d(TAG, "BOOF: DEBUG 2 CHECK!");
-                QrCodeDetector<GrayU8> detector = FactoryFiducial.qrcode(null, GrayU8.class);
-                Log.d(TAG, "BOOF: DEBUG 3 CHECK!");
-                detector.process(image);
-                Log.d(TAG, "BOOF: DEBUG 4 CHECK!");
-                List<QrCode> detections = detector.getDetections();
-                Log.d(TAG, "BOOF: DEBUG 5 CHECK!");
-                Log.d(TAG, "BOOF: DEBUG 5.5 CHECK!");
-                if (detections != null) {
-                    for(QrCode qr : detections) {
-                        Log.d(TAG, "BOOF: DEBUG 6 CHECK!");
-                        if (qr.message.length() > 0) {
-                            Log.d(TAG, "BOOF: DEBUG 7 CHECK!");
-                            api.judgeSendDiscoveredQR(nomer, qr.message);
-                            QR_FOUND = true;
-                            TEST_FOUND = true;
-                            QR_LIST.add(qr.message);
-                            Log.d(TAG, "Posisi P3: " + qr.message);
-                        }
-                        else {
-                            Log.d(TAG, "QR ERROR: NOT FOUND!");
-                        }
+            GrayU8 image = ConvertBitmap.bitmapToGray(resize, (GrayU8)null, null);
+            Log.d(TAG, "BOOF: DEBUG 1 CHECK!");
+            Log.d(TAG, "BOOF: DEBUG 2 CHECK!");
+            QrCodeDetector<GrayU8> detector = FactoryFiducial.qrcode(null, GrayU8.class);
+            Log.d(TAG, "BOOF: DEBUG 3 CHECK!");
+            detector.process(image);
+            Log.d(TAG, "BOOF: DEBUG 4 CHECK!");
+            List<QrCode> detections = detector.getDetections();
+            Log.d(TAG, "BOOF: DEBUG 5 CHECK!");
+            Log.d(TAG, "BOOF: DEBUG 5.5 CHECK!");
+            if (detections != null) {
+                for(QrCode qr : detections) {
+                    Log.d(TAG, "BOOF: DEBUG 6 CHECK!");
+                    if (qr.message.length() > 0) {
+                        Log.d(TAG, "BOOF: DEBUG 7 CHECK!");
+                        api.judgeSendDiscoveredQR(nomer, qr.message);
+                        QR_FOUND = true;
+                        QR_LIST.add(qr.message);
+                        Log.d(TAG, "Posisi P3: " + qr.message);
+                    }
+                    else {
+                        Log.d(TAG, "QR ERROR: NOT FOUND!");
                     }
                 }
-                else {
-                    Log.d(TAG, "DETECTION NOT FOUND!");
-                }
-
-            } catch (Exception e) {
-                Log.d(TAG, "EXCEPTION OCCURRED!: " + e.getMessage());
+            }
+            else {
+                Log.d(TAG, "DETECTION NOT FOUND!");
             }
             bBoof.recycle();
             resize.recycle();
             resize = null;
             rotate++;
             ++loopCheckQRCount;
-        }
-
-        if (!TEST_FOUND) {
-            selfPosition(nomer);
-            Log.d(TAG, "Trying with ZBar..");
-            readQR_ZBar(nomer, camBoof);
         }
     }
 
@@ -317,7 +325,6 @@ public class YourService extends KiboRpcService {
                 moveRobot(POINT_P12, QUAT_P12);
                 break;
             case 2:
-                moveRobot(POINT_P13_1, QUAT_P13_1);
                 moveRobot(POINT_P13, QUAT_P13);
                 break;
             case 3:
@@ -332,78 +339,6 @@ public class YourService extends KiboRpcService {
         }
     }
 
-    public void readQR_ZBar(int zbar_number, String cam){
-        ImageScanner scanner = new ImageScanner();
-        int LOOP_CHECKQR_MAX = 4;
-        int loopCheckQRCount = 0;
-        int rotate = 0;
-        boolean QR_ZBAR_FOUND = false;
-        Log.d(TAG, "ZBAR: DEBUG 1 CHECK!");
-        scanner.setConfig(0, Config.ENABLE, 0);
-        scanner.setConfig(Symbol.QRCODE, Config.ENABLE, 1);
-        Log.d(TAG, "ZBAR: DEBUG 2 CHECK!");
-        while (loopCheckQRCount < LOOP_CHECKQR_MAX && !QR_ZBAR_FOUND) {
-            Bitmap bmapzbar = null;
-            if (cam.equals("dock")) {
-                bmapzbar = api.getBitmapDockCam();
-                if (rotate == 1) bmapzbar = RotateBitmap(bmapzbar, 1);
-                if (rotate == 2) bmapzbar = RotateBitmap(bmapzbar, 2);
-                if (rotate == 3) bmapzbar = RotateBitmap(bmapzbar, 3);
-
-            }
-            else if (cam.equals("nav")) {
-                bmapzbar = api.getBitmapNavCam();
-                if (rotate == 1) bmapzbar = RotateBitmap(bmapzbar, 1);
-                if (rotate == 2) bmapzbar = RotateBitmap(bmapzbar, 2);
-                if (rotate == 3) bmapzbar = RotateBitmap(bmapzbar, 3);
-            }
-            if (bmapzbar == null) {
-                Log.d(TAG, "ZBAR: BITMAP IS NULL!");
-            }
-            Bitmap grayscaled = toGrayscale(bmapzbar);
-            Bitmap scaled = getResizedBitmap(grayscaled, height/2, width/2);
-            int[] intArray = new int[bmapzbar.getWidth() * bmapzbar.getHeight()];
-            int wdth = scaled.getWidth();
-            int hght = scaled.getHeight();
-            Log.d(TAG, "New width bmp: " + wdth);
-            Log.d(TAG, "New height bmp: " + hght);
-            Log.d(TAG, "ZBAR: DEBUG 3 CHECK!");
-            try {
-                scaled.getPixels(intArray, 0, wdth, 0, 0, wdth, hght);
-                Log.d(TAG, "ZBAR: DEBUG 4 CHECK!");
-                Image barcode = new Image(wdth, hght, "RGB4");
-                Log.d(TAG, "ZBAR: DEBUG 5 CHECK!");
-                barcode.setData(intArray);
-                Log.d(TAG, "ZBAR: DEBUG 6 CHECK!");
-                int result_zbar = scanner.scanImage(barcode.convert("Y800"));
-                Log.d(TAG, "ZBAR: DEBUG 7 CHECK!");
-                if (result_zbar != 0) {
-                    for (Symbol symbol_zbar : scanner.getResults()) {
-                        Log.d(TAG, "ZBAR: DEBUG 8 CHECK!");
-                        String qrContent_zbar = symbol_zbar.getData();
-                        api.judgeSendDiscoveredQR(zbar_number, qrContent_zbar);
-                        QR_ZBAR_FOUND = true;
-                        TEST_FOUND = true;
-                        QR_LIST.add(qrContent_zbar);
-                        Log.d(TAG, "Found QR Content: " + qrContent_zbar);
-                    }
-                }
-                else {
-                    Log.d(TAG, "QR ERROR: NOT FOUND!");
-                }
-            } catch (Exception e) {
-                Log.d(TAG, "EXCEPTION OCCURED! " + e.getMessage());
-            }
-            grayscaled.recycle();
-            scaled.recycle();
-            scaled = null;
-            grayscaled = null;
-            bmapzbar.recycle();
-            ++loopCheckQRCount;
-            rotate++;
-        }
-    }
-
     public Bitmap RotateBitmap(Bitmap src, int no) {
         float angle = 0;
         if (no == 1) angle = 90;
@@ -413,21 +348,6 @@ public class YourService extends KiboRpcService {
         Matrix matriks = new Matrix();
         matriks.postRotate(angle);
         return Bitmap.createBitmap(src, 0, 0, src.getWidth(), src.getHeight(), matriks, true);
-    }
-
-    public Bitmap toGrayscale(Bitmap bmpOriginal) {
-        int width, height;
-        height = bmpOriginal.getHeight();
-        width = bmpOriginal.getWidth();
-        Bitmap bmpGrayscale = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
-        Canvas c = new Canvas(bmpGrayscale);
-        Paint paint = new Paint();
-        ColorMatrix cm = new ColorMatrix();
-        cm.setSaturation(0);
-        ColorMatrixColorFilter f = new ColorMatrixColorFilter(cm);
-        paint.setColorFilter(f);
-        c.drawBitmap(bmpOriginal, 0, 0, paint);
-        return bmpGrayscale;
     }
 
     public Bitmap getResizedBitmap(Bitmap bm, int newHeight, int newWidth) {
